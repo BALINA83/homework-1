44a641c16eb404204f2329754ed135ab71bd2f4e
Alina Bezsonova <alina.bessonova83@gmail.com>
2026-06-07 14:16:57 +0200

    Fix build

--- vite-starter-markup-template-main/vite.config.js
+++ vite-starter-markup-template-main/vite.config.js
@@ -4,53 +4,52 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import glob from 'fast-glob'
 import { fileURLToPath } from 'url'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// you can use our path for your project
-const rootPath = '/homework-1/'
-// example: const rootPath = '/my-path/'
+const isProd = process.env.NODE_ENV === 'production'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-	plugins: [
-		ViteImageOptimizer({
-			svg: {
-				plugins: [
-					'removeDoctype',
-					'removeXMLProcInst',
-					'minifyStyles',
-					'sortAttrs',
-					'sortDefsChildren',
-				],
-			},
-			png: {
-				quality: 70,
-			},
-			jpeg: {
-				quality: 70,
-			},
-			jpg: {
-				quality: 70,
-			}
-		}),
-		{
-			...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
-				destination: './src/img/webp/',
-				plugins: [
-					imageminWebp({ quality: 70 })
-				]
-			}),
-			apply: 'serve',
-		}
-	],
-	build: {
-		rollupOptions: {
-			input: Object.fromEntries(
-				glob.sync(['./*.html', './pages/**/*.html']).map(file => [
-					path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
-					fileURLToPath(new URL(file, import.meta.url))
-				])
-			)
-		},
-	},
-	base: `${rootPath}`,
+  base: isProd ? '/homework-1/' : '/',
+
+  plugins: [
+    ViteImageOptimizer({
+      svg: {
+        plugins: [
+          'removeDoctype',
+          'removeXMLProcInst',
+          'minifyStyles',
+          'sortAttrs',
+          'sortDefsChildren',
+        ],
+      },
+      png: { quality: 70 },
+      jpeg: { quality: 70 },
+      jpg: { quality: 70 },
+    }),
+
+    {
+      ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
+        destination: './src/img/webp/',
+        plugins: [imageminWebp({ quality: 70 })],
+      }),
+      apply: 'serve',
+    },
+  ],
+
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob.sync(['./*.html', './pages/**/*.html']).map(file => [
+          path.relative(
+            __dirname,
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
+    },
+  },
 })
